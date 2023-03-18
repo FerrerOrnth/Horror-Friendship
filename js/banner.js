8417e94d2c32fa28cff6ca618921f17306fe06f4
Claudio Alberto Ferrer Ornth <103849449+FerrerOrnth@users.noreply.github.com>
2023-03-18 15:03:33 -0300

    Add files via upload
    Primer commit

---
+++ js/banner.js
@@ -0,0 +1,12 @@
+
+function mostrar() {
+    div = document.getElementById('flotante');
+    div.style.display = '';
+}
+
+function cerrar() {
+    div = document.getElementById('flotante');
+    if (div.style.display = '') {
+      div.style.display = 'none';
+    }
+}
